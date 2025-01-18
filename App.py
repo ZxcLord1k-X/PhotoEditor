1d7be2fe1d9dbfc7b747d4afc368700e89c5b1f2
ZxcLord1k-X <dima.stasuk64@gmail.com>
2025-01-18 15:33:04 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,15 +1,16 @@
 import os
 
-from PyQt5.QtWidgets import (
-QApplication, QWidget, QListWidget,
-QHBoxLayout, QVBoxLayout,
-QPushButton, QLabel, QFileDialog)
+from PyQt6.QtWidgets import (
+        QApplication, QWidget, QListWidget,
+        QHBoxLayout, QVBoxLayout, QFileDialog,
+        QPushButton, QLabel)
 
-from QSS import QSS
+from qss import QSS
 
-from PyQt5.QtCore import Qt # потрібна константа Qt.KeepAspectRatio для зміни розмірів із збереженням пропорцій
-from PyQt5.QtGui import QPixmap # оптимізована для показу на екрані картинка
+from PyQt6.QtCore import Qt # потрібна константа Qt.KeepAspectRatio для зміни розмірів із збереженням пропорцій
+from PyQt6.QtGui import QPixmap # оптимізована для показу на екрані картинка
 from PIL import Image
+from PIL import ImageFilter
 
 app = QApplication([])
 win = QWidget()
@@ -29,7 +30,7 @@ btn_right = QPushButton("Управо")
 btn_flip = QPushButton("Дзеркало")
 btn_sharp = QPushButton("Різкість")
 btn_bw = QPushButton("Ч/Б")
-
+btn_texture = QPushButton("рамка")
 
 row = QHBoxLayout()          # Основний рядок
 col1 = QVBoxLayout()         # ділиться на два стовпці
@@ -39,6 +40,7 @@ col1.addWidget(lw_files)     # та список файлів
 col2.addWidget(lb_image, 95) # у другому - картинка
 row_tools = QHBoxLayout()    # та рядок кнопок
 row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_texture)
 row_tools.addWidget(btn_right)
 row_tools.addWidget(btn_flip)
 row_tools.addWidget(btn_sharp)
@@ -53,79 +55,122 @@ win.setLayout(row)
 
 win.show()
 
-work_dir = ""
-def filter(files, extentions):
-    result = []
-    for filename in files:
-        for ext in extentions:
-            if filename.endswith(ext):
-                result.append(filename)
-    return result
+workdir = ""
+
+def filter(files, extensions):
+   result = []
+   for filename in files:
+       for ext in extensions:
+           if filename.endswith(ext):
+               result.append(filename)
+   return result
+
 
 def chooseWorkdir():
-    global work_dir
-    work_dir = QFileDialog.getExistingDirectory()
+   global workdir
+   workdir = QFileDialog.getExistingDirectory()
 
-def showFileNamesList():
-    extentions = [".jpg", ".jpeg", ".png", ".svg", ".gif", ".bmp"]
-    chooseWorkdir()
-    filenames = filter(os.listdir(work_dir), extentions)
-    lw_files.clear()
-    for filename in filenames:
-        lw_files.addItem(filename)
+
+def showFilenamesList():
+   extensions = [".jpg",".jpeg", ".png", ".gif", ".bmp"]
+   chooseWorkdir()
+   filenames = filter(os.listdir(workdir), extensions)
+   lw_files.clear()
+   for filename in filenames:
+       lw_files.addItem(filename)
 
 class ImageProcessor:
-    def __init__(self):
-        self.image = None
-        self.dir = None
-        self.filename = None
-        self.save_dir = "Modified/"
+        def __init__(self):
+            self.image = None
+            self.dir = None
+            self.filename = None
+            self.save_dir = "Modified"
 
-    def loadImage(self, dir, filename):
-        ''' під час завантаження запам'ятовуємо шлях та ім'я файлу '''
-        self.dir = dir
-        self.filename = filename
-        image_path = os.path.join(dir, filename)
-        self.image = Image.open(image_path)
+        def showImage(self, path):
+                lb_image.hide()
+                pixmapimage = QPixmap(path)
+                w, h = lb_image.width(), lb_image.height()
+                pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+                lb_image.setPixmap(pixmapimage)
+                lb_image.show()
 
-    def saveImage(self):
-        ''' зберігає копію файлу у підпапці '''
-        path = os.path.join(self.dir, self.save_dir)
-        if not (os.path.exists(path) or os.path.isdir(path)):
-            os.mkdir(path)
-        image_path = os.path.join(path, self.filename)
-        self.image.save(image_path)
+        def loadImage(self, dir, filename):
+            ''' під час завантаження запам'ятовуємо шлях та ім'я файлу '''
+            self.dir = dir
+            self.filename = filename
+            image_path = os.path.join(dir, filename)
+            self.image = Image.open(image_path)
 
-    def showImage(self, path):
-        lb_image.hide()  # Ховає елемент `lb_image`, щоб під час зміни зображення користувач не бачив проміжного стану.
-        pixmapimage = QPixmap(path)  # Завантажує зображення з вказаного шляху (`path`) у об'єкт `QPixmap`.
-        w, h = lb_image.width(), lb_image.height()  # Зберігає поточну ширину (`w`) і висоту (`h`) віджета `lb_image`.
-        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)  # Масштабує зображення, щоб воно відповідало розмірам `lb_image`, зберігаючи пропорції.
-        lb_image.setPixmap(pixmapimage)  # Встановлює масштабоване зображення в якості вмісту для віджета `lb_image`.
-        lb_image.show()  # Показує елемент `lb_image` з новим зображенням.
+        def saveImage(self):
+            ''' зберігає копію файлу у підпапці '''
+            path = os.path.join(self.dir, self.save_dir)
+            if not (os.path.exists(path) or os.path.isdir(path)):
+                os.mkdir(path)
+            image_path = os.path.join(path, self.filename)
+            self.image.save(image_path)
 
-    def do_bw(self):
-        self.image = self.image.convert("L")
-        self.saveImage()
-        image_path = os.path.join(
-            self.dir, self.save_dir, self.filename)
-        self.showImage(image_path)
+        def do_bw(self):
+                self.image = self.image.convert("L")
+                self.saveImage()
+                image_path = os.path.join(self.dir, self.filename)
+                self.showImage(image_path)
+
+        def do_left(self):
+            self.image = self.image.transpose(Image.ROTATE_90)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+
+        def do_right(self):
+            self.image = self.image.transpose(Image.ROTATE_270)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+
+        def do_flip(self):
+            self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+
+        def do_sharpen(self):
+            self.image = self.image.filter(ImageFilter.SHARPEN)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+
+        def add_texture(self):
+            texture = Image.open("pink.png")
+            width, height = self.image.size
+            texture = texture.resize((width, height))
+            self.image.paste(texture, (0, 0), texture)
+
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
 
 
-def showChosenImage():  # Оголошення функції `showChosenImage`, яка відображає вибране зображення з переліку файлів.
-    if lw_files.currentRow() >= 0:  # Перевіряє, чи обраний будь-який елемент у списку `lw_files` (чи є активний рядок).
-        filename = lw_files.currentItem().text()  # Отримує текст вибраного елемента списку (ім'я файлу).
-        workimage.loadImage(work_dir, filename)  # Завантажує вибране зображення, передаючи робочу директорію та ім'я файлу.
-        image_path = os.path.join(workimage.dir, workimage.filename)  # Створює повний шлях до зображення, поєднуючи директорію та ім'я файлу.
-        workimage.showImage(image_path)  # Викликає метод `showImage`, щоб відобразити зображення за вказаним шляхом.
+def showChosenImage():
+        if lw_files.currentRow() >= 0:
+                filename = lw_files.currentItem(). Text()
+                workimage.loadImage(workdir, filename)
+                image_paht = os.path.join(workimage.dir, workimage.filename)
+                workimage.showImage(image_paht)
 
 
-btn_dir.clicked.connect(showFileNamesList)
+btn_dir.clicked.connect(showFilenamesList)
 
-workimage = ImageProcessor() #поточне робоче зображення для роботи
+workimage = ImageProcessor()
 
 lw_files.currentRowChanged.connect(showChosenImage)
 btn_bw.clicked.connect(workimage.do_bw)
+
+btn_left.clicked.connect(workimage.do_left)
+btn_right.clicked.connect(workimage.do_right)
+btn_sharp.clicked.connect(workimage.do_sharpen)
+btn_flip.clicked.connect(workimage.do_flip)
+btn_texture.clicked.connect(workimage.add_texture)
+
 
 app.exec()
 
